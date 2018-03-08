9146f7ce61038bdf0f407c499a9486db44d7ca7a
elektronikman3492 <jedrekr07@gmail.com>
2018-03-08 15:22:16 +0100

    Update hd44780.h

--- HD44780.X/hd44780.h
+++ HD44780.X/hd44780.h
@@ -1,20 +1,25 @@
 #ifndef HD44780_H
 #define	HD44780_H
 
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
 //przypisanie nazw dla portow
-#define DB7tris TRISBbits.TRISB7 /*przypisanie nazwy DB7tris dla RB7 jako TRISBbits.TRISB7 aby mozna bylo ustawic,
+#define DB7tris TRISD5 /*przypisanie nazwy DB7tris dla RB7 jako TRISBbits.TRISB7 aby mozna bylo ustawic,
 czy ma pracowa? jako wejscie lub wyjscie*/
-#define DB7 LATBbits.LATB7 //przypisanie nazwy DB7 dla RB7 jako LATBbits.LATB7 aby wystawiac na nim stan logiczny
-#define DB6tris TRISBbits.TRISB6
-#define DB6 LATBbits.LATB6
-#define DB5tris TRISBbits.TRISB5
-#define DB5 LATBbits.LATB5
-#define DB4tris TRISBbits.TRISB4
-#define DB4 LATBbits.LATB4
-#define Etris TRISBbits.TRISB3
-#define E LATBbits.LATB3 //Enable
-#define RStris TRISBbits.TRISB2
-#define RS LATBbits.LATB2
+#define DB7 PORTDbits.RD5 //przypisanie nazwy DB7 dla RB7 jako LATBbits.LATB7 aby wystawiac na nim stan logiczny
+#define DB6tris TRISD4
+#define DB6 PORTDbits.RD4
+#define DB5tris TRISD3
+#define DB5 PORTDbits.RD3
+#define DB4tris TRISD2
+#define DB4 PORTDbits.RD2
+#define Etris TRISD1
+#define E PORTDbits.RD1 //Enable
+#define RStris TRISD0
+#define RS PORTDbits.RD0
 
 //definicja komend
 #define LCD_CLEAR 1
@@ -25,6 +30,8 @@ czy ma pracowa? jako wejscie lub wyjscie*/
 #define DISP_ON_CUROSOR_OFF_BLINK_ON 13
 #define _4_BIT_2_LINES_FONT_5X8 40
 #define _2_ROW 192
+#define _3_ROW 148
+#define _4_ROW 212
 #define RETURN_HOME 2
 #define CURSOR_RIGHT 20
 #define CURSOR_LEFT 16
@@ -43,7 +50,9 @@ void znaki_pl_i_stopien();
 
 
     void Init_lcd() {
-
+        for (unsigned char w=0; w<10; w++) {
+            __delay_ms(10);
+        }
         //ustawnienie portow jako wyjscia
         DB7tris = 0;
         DB6tris = 0;
@@ -62,16 +71,13 @@ void znaki_pl_i_stopien();
 
 
 /******************************************************************************/
-
-
-//Ustawniwnie wy?wietlacza w tryb 4 bitowy
-        E = 1;
-        DB7 = 0;
-        DB6 = 0;
-        DB5 = 1;
-        DB4 = 0;
-        E = 0;
+        LcdCmd(0x03);
         __delay_ms(5);
+        LcdCmd(0x03);
+        __delay_ms(5);
+        LcdCmd(0x03);
+        __delay_us(150);
+        LcdCmd(0x02); //Ustawniwnie wy?wietlacza w tryb 4 bitowy
 
     }
 
@@ -115,13 +121,18 @@ void znaki_pl_i_stopien();
 
     void LcdText(unsigned char x, unsigned char y, const char *text) {
         LcdCmd(RETURN_HOME);
-        if (y == 2) { //jesli y=2 przejdz do 2 linii
+        if (y == 1) { //jesli y=1 przejdz do 2 linii
             LcdCmd(_2_ROW);
         }
-        else {
+        else if (y == 2) {
+            LcdCmd(_3_ROW);
+        } else if (y == 3) {
+            LcdCmd(_4_ROW);
+        } else {
             LcdCmd(RETURN_HOME);
+            __delay_ms(2);
         }
-        
+
         for (int i=0; i<x; i++) { //przesuwaj kursor w prawo o tyle pol ile wynosi x
             LcdCmd(CURSOR_RIGHT);
         }
@@ -135,16 +146,23 @@ void znaki_pl_i_stopien();
 
     void LcdClr(void) {
         LcdCmd(LCD_CLEAR);
-        LcdCmd(DISP_ON_CURSOR_OFF_BLINK_OFF);
+        __delay_ms(2);
+        LcdCmd(DISP_ON_CURSOR_OFF_BLINK_OFF); //wy?wietlacz w??czony, kursor wy??czony i nie miga
     }
 
     void SetCursor(unsigned char x, unsigned char y) {
         LcdCmd(RETURN_HOME);
-        if (y == 2) { //jesli y=2 przejdz do 2 linii
+        __delay_ms(2);
+        if (y == 1) { //jesli y=2 przejdz do 2 linii
             LcdCmd(_2_ROW);
         }
-        else {
+        else if(y == 2) {
+            LcdCmd(_3_ROW);
+        } else if(y == 3) {
+            LcdCmd(_4_ROW);
+        } else {
             LcdCmd(RETURN_HOME);
+            __delay_ms(2);
         }
 
         for (int i=0; i<x; i++) { //przesuwaj kursor w prawo o tyle pol ile wynosi x
@@ -161,7 +179,7 @@ void znaki_pl_i_stopien();
         char data5[8] = {12,4,6,12,4,4,14,0};
         char data6[8] = {2,4,22,25,17,17,17,0};
         char data7[8] = {2,4,14,17,17,17,14,0};
-        char data8[8] = {2,4,14,16,14,1,30,0};
+        char data8[8] = {2,4,14,16,14,1,30,0}; //si
         //char data9[8] = {2,4,31,2,4,8,31,0};
         //char data10[8] = {4,0,31,2,4,8,31,0};
 
@@ -196,8 +214,14 @@ void znaki_pl_i_stopien();
         LcdCmd(120);
         for (unsigned char i=0; i<8; i++) {
             LcdData(data8[i]);
-        }   
+        }
     }
+
+
+
+#ifdef	__cplusplus
+}
+#endif
 
 #endif	/* HD44780_H */
 
